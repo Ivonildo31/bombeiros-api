9906fc3ddc97723d90afd6228f6afb27870fc756
vizeke <vizeke@gmail.com>
2016-10-05 19:11:34 -0300

    Adiciona o request path

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,9 +7,9 @@ import { IRethinkDBConfig } from '../config/rethinkdb'
 export namespace main {
     export const callRoutes = (app: express.Application, rethinkdbconfig: IRethinkDBConfig): express.Application => {
         let models = new Model(rethinkdbconfig)
-        app.use('/users', new UserRouter(models).getRouter())
-        app.use('/alerts', new AlertRouter(models).getRouter())
-        app.use('/', (req,res,nex) => res.json('ok'))
+        app.use('/cbmes/users', new UserRouter(models).getRouter())
+        app.use('/cbmes/alerts', new AlertRouter(models).getRouter())
+        app.use('/cbmes', (req,res,nex) => res.json('ok'))
         return app
     }
 }
